ef4a25ddf57d440cc542a95bc8a8c3b673890fd5
Ash Rana <ashrana1993@gmail.com>
2020-11-24 02:49:29 -0600

    ss

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -30,10 +30,10 @@ export const Contact = (props) => {
                     <div className="connectWM">Connect with me!</div>
 
                     <div className="connectLogos">
-                        <a href="https://www.facebook.com/ash.rana.19/" target="_blank" rel="noopener noreferrer"><img className="logoImg" src="/assets/images/facebook.png" alt="" /></a>
-                        <a href="https://github.com/ARANA107" target="_blank" rel="noopener noreferrer"><img className="logoImg" src="/assets/images/github.png" alt="" /></a>
-                        <a href="mailto:ashrana1993@gmail.com" target="_blank" rel="noopener noreferrer"><img className="logoImg" src="/assets/images/gmail.png" alt="" /></a>
-                        <a href="https://www.linkedin.com/in/ashrana1993/" target="_blank" rel="noopener noreferrer"><img className="logoImg" src="/assets/images/linkedin.png" alt="" /></a>
+                        <a href="https://www.facebook.com/ash.rana.19/" target="_blank" rel="noopener noreferrer"><img className="logoImg" src="assets/images/facebook.png" alt="" /></a>
+                        <a href="https://github.com/ARANA107" target="_blank" rel="noopener noreferrer"><img className="logoImg" src="assets/images/github.png" alt="" /></a>
+                        <a href="mailto:ashrana1993@gmail.com" target="_blank" rel="noopener noreferrer"><img className="logoImg" src="assets/images/gmail.png" alt="" /></a>
+                        <a href="https://www.linkedin.com/in/ashrana1993/" target="_blank" rel="noopener noreferrer"><img className="logoImg" src="assets/images/linkedin.png" alt="" /></a>
                     </div>
                 </div>
             </div>
