fe7898678ef5b6d18bbfb8daaf53e8f3ac37bfd2
Ash Rana <ashrana1993@gmail.com>
2020-11-24 03:19:35 -0600

    ss

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -35,7 +35,7 @@ export const Contact = (props) => {
                     <div className="connectWM">Connect with me!</div>
 
                     <div className="connectLogos">
-                        <a href="https://www.facebook.com/ash.rana.19/" target="_blank" rel="noopener noreferrer"><img className="logoImg" src="assets/images/facebook.png" alt="" /></a>
+                        <a href="https://www.facebook.com/ash.rana.19/" target="_blank" rel="noopener noreferrer"><img className="logoImg" src={facebook} alt="" /></a>
                         <a href="https://github.com/ARANA107" target="_blank" rel="noopener noreferrer"><img className="logoImg" src={github} alt="" /></a>
                         <a href="mailto:ashrana1993@gmail.com" target="_blank" rel="noopener noreferrer"><img className="logoImg" src={gmail} alt="" /></a>
                         <a href="https://www.linkedin.com/in/ashrana1993/" target="_blank" rel="noopener noreferrer"><img className="logoImg" src={linkedin} alt="" /></a>
